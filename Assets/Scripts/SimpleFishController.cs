7164c648d0dafa735d536c7a24e6b93acb58a960
g <gregheff@gmail.com>
2020-02-03 20:52:45 -0500

    Week 2

---
+++ Assets/Scripts/SimpleFishController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFishController : MonoBehaviour
+{
+    public float moveSpeed = .05f;
+    public float rotationSpeed = .5f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0,0,moveSpeed));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0,0,-moveSpeed));
+        }
+    
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -rotationSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, rotationSpeed, 0));
+        }        
+    }
+}
