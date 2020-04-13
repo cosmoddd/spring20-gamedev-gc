fdea5448e5748b8ae7fb945de9950d9f4fa46d21
g <gregheff@gmail.com>
2020-04-13 00:14:49 -0400

    animation stuff

---
+++ Assets/AnimationChooser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationChooser : MonoBehaviour
+{
+    public  Animator thisAnimator;
+
+    void Start()
+    {
+        thisAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            thisAnimator.SetTrigger("Eric");
+        }
+    }
+}
